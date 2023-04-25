88b1b9c92dac78c2f18d7c686ed72f39d57bbfa7
EunjiLee <eunji.lee@uzh.ch>
2023-04-25 16:03:57 +0200

    pincode #70

--- src/components/views/PinCode.js
+++ src/components/views/PinCode.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {api, handleError} from 'helpers/api';
 //import {useHistory} from 'react-router-dom';
 import BaseContainer from "components/ui/BaseContainer";
@@ -7,6 +7,9 @@ import "styles/views/MultiPlayer.scss";
 import "styles/views/PinCode.scss";
 import '../pictures/2.jpg';
 import logo from '../pictures/Logo.jpg';
+import PinInput from "react-pin-input";
+import {useHistory} from 'react-router-dom';
+import {Button} from 'components/ui/Button';
 
 const Player = ({user}) => (
     <div className="player container">
@@ -19,58 +22,22 @@ Player.propTypes = {
 };
 
 const PinCode = () => {
-    // use react-router-dom's hook to access the history
-    //const history = useHistory();
+    const history = useHistory();
+    const [pincode, setPincode] = useState(null);
+    const userId = localStorage.getItem('userId');
 
-    // define a state variable (using the state hook).
-    // if this variable changes, the component will re-render, but the variable will
-    // keep its value throughout render cycles.
-    // a component can have as many state variables as you like.
-    // more information can be found under https://reactjs.org/docs/hooks-state.html
-    const [users, setUsers] = useState(null);
-    console.log(users)
-
-
-
-
-    // the effect hook can be used to react to change in your component.
-    // in this case, the effect hook is only run once, the first time the component is mounted
-    // this can be achieved by leaving the second argument an empty array.
-    // for more information on the effect hook, please see https://reactjs.org/docs/hooks-effect.html
-    useEffect(() => {
-        // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
-        async function fetchData() {
-            try {
-                const response = await api.get('/users');
-
-                // delays continuous execution of an async operation for 1 second.
-                // This is just a fake async call, so that the spinner can be displayed
-                // feel free to remove it :)
-                await new Promise(resolve => setTimeout(resolve, 1000));
-
-                // Get the returned users and update the state.
-                setUsers(response.data);
-
-                // This is just some data for you to see what is available.
-                // Feel free to remove it.
-                console.log('request to:', response.request.responseURL);
-                console.log('status code:', response.status);
-                console.log('status text:', response.statusText);
-                console.log('requested data:', response.data);
-
-                // See here to get more data.
-                console.log(response);
-            } catch (error) {
-                console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
-                console.error("Details:", error);
-                alert("Something went wrong while fetching the users! See the console for details.");
-            }
+    const joinGame = async () => {
+        try {
+            const requestBody = JSON.stringify({id:userId});
+            const response = await api.post('/lobbies/joinGame/'+pincode, requestBody)
+            console.log(response);
+            const gameId = response.data.gameId;
+            history.push(`/lobby/` + gameId);
+        } catch (error) {
+            console.log(userId);
+            alert(`Something went wrong during the login: \n${handleError(error)}`);
         }
-
-        fetchData();
-    }, []);
-
-
+    };
 
     return (
         <BaseContainer className="multiplayer container">
@@ -85,13 +52,12 @@ const PinCode = () => {
             </div>
             <h1 className="pin-title">Enter PIN here</h1>
             <div className="pin-code">
-                <input type="number" maxLength="1" autoFocus/>
-                    <input type="number" maxLength="1"/>
-                        <input type="number" maxLength="1"/>
-                            <input type="number" maxLength="1"/>
+            <PinInput length={6} focus type="text" inputMode="text" value={pincode} onChange={a => setPincode(a.toLowerCase())}/>
+     
             </div>
-
-            <button className="pin-code-button">Join Game</button>
+            <Button className="pin-code-button" onClick={() => joinGame()}>
+            Join Game
+        </Button>
         </BaseContainer>
     );
 }
