e6f734a30d441cb7aedcf8a2a6b53c57658b0c70
EunjiLee <eunji.lee@uzh.ch>
2023-04-25 17:42:46 +0200

    update the lobby players

--- src/components/views/Lobby.js
+++ src/components/views/Lobby.js
@@ -9,148 +9,55 @@ import logo from '../pictures/Logo.jpg';
 import Player from "../../models/Player";
 
 
-const Pleyer = ({user}) => (
+const Players = ({user}) => (
     <div className="player container">
-        <div className="player username">{user.username}</div>
+        <div className="player username">{user.playerName}</div>
     </div>
 );
 
-Pleyer.propTypes = {
-    user: PropTypes.object
-};
-
-
-
-let p1 = {
-    username: "Timo",
-    points: 1615,
-    rank: 1
-}
-
-let p2 = {
-    username: "Euni",
-    points: 1315,
-    rank: 3
-}
-
-let p3 = {
-    username: "Tiago",
-    points: 1523,
-    rank: 2
-}
-
-let p4 = {
-    username: "Yuqing",
-    points: 1267,
-    rank: 4
-}
-
-let p5 = {
-    username: "Laurent",
-    points: 12,
-    rank: 5
-}
-
-let player1 = new Player(p1);
-let player2 = new Player(p2);
-let player3 = new Player(p3);
-let player4 = new Player(p4);
-let player5 = new Player(p5);
-let players = [player1, player2, player3, player4, player5];
-
 const Lobby = () => {
-    // use react-router-dom's hook to access the history
-
-    //const history = useHistory();
-
-    // define a state variable (using the state hook).
-    // if this variable changes, the component will re-render, but the variable will
-    // keep its value throughout render cycles.
-    // a component can have as many state variables as you like.
-    // more information can be found under https://reactjs.org/docs/hooks-state.html
+    // const history = useHistory();
     const gameId = localStorage.getItem('gameId');
-
-
-
-    const [users, setUsers] = useState(null);
-    console.log(users)
-
-    console.log('/lobbies/'+gameId);
 
     const [rounds, setRounds] = useState(null);
     const [pin, setPin] = useState(null);
     const [category, setCategory] = useState(null);
     const [gameMode, setGameMode] = useState(null);
+    const [players, setPlayers] = useState(null);
 
     localStorage.setItem('currentRound', '1');
 
-    async function doSettings() {
-        try {
-            const settings = await api.get('/lobbies/'+gameId);
-            console.log(settings.data);
-            setGameMode(settings.data.gameMode);
-            setRounds(settings.data.rounds);
-            setCategory(settings.data.category);
-            setPin(settings.data.gamePIN);
-            console.log(rounds);
-        } catch (error) {
-            console.log('not functioning');
-        }
-    }
-
     useEffect(() => {
-        doSettings();
-    });
-
-    console.log(rounds);
-
-
-    // the effect hook can be used to react to change in your component.
-    // in this case, the effect hook is only run once, the first time the component is mounted
-    // this can be achieved by leaving the second argument an empty array.
-    // for more information on the effect hook, please see https://reactjs.org/docs/hooks-effect.html
-    useEffect(() => {
-        // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
-        async function fetchData() {
-            try {
-                const response = await api.get('/users');
-
-                // delays continuous execution of an async operation for 1 second.
-                // This is just a fake async call, so that the spinner can be displayed
-                // feel free to remove it :)
-                await new Promise(resolve => setTimeout(resolve, 1000));
-
-                // Get the returned users and update the state.
-                setUsers(response.data);
-
-                // This is just some data for you to see what is available.
-                // Feel free to remove it.
-                console.log('request to:', response.request.responseURL);
-                console.log('status code:', response.status);
-                console.log('status text:', response.statusText);
-                console.log('requested data:', response.data);
-
-                // See here to get more data.
-                console.log(response);
-            } catch (error) {
-                console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
-                console.error("Details:", error);
-                alert("Something went wrong while fetching the users! See the console for details.");
+        const interval = setInterval(() => {
+            async function fetchData() {
+                try {
+                    const response = await api.get('/lobbies/'+gameId);
+                    setGameMode(response.data.gameMode);
+                    setRounds(response.data.rounds);
+                    setCategory(response.data.category);
+                    setPin(response.data.gamePIN);
+                    setPlayers(response.data.players);
+                } catch (error) {
+                    alert(`Something went wrong while fetching the game: \n${handleError(error)}`);
+                }
             }
-        }
-
-        fetchData();
+            fetchData();
+        }, 1000);
+        return () => clearInterval(interval);
     }, []);
 
-    let content = (
+    let content = (<div></div>);
+    if (players) {
+        content = (
             <div className="game">
                 <ul className="game user-list">
                     {players.map(user => (
-                        <Pleyer user={user} key={user.username}/>
+                        <Players user={user} key={user.id}/>
                     ))}
                 </ul>
             </div>
-    );
+        );
+    }
 
     return (
         <BaseContainer className="multiplayer container">
