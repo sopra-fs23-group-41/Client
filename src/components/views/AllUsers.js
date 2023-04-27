9e689f0f55c1335e251c7407c0c7cadecae84990
EunjiLee <eunji.lee@uzh.ch>
2023-04-27 10:14:18 +0200

    show all user list

--- src/components/views/AllUsers.js
+++ src/components/views/AllUsers.js
@@ -1,18 +1,19 @@
 import {useEffect, useState} from 'react';
 import {api, handleError} from 'helpers/api';
-import {Link, useHistory} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import "styles/views/Landing.scss";
+import "styles/views/AllUsers.scss";
 import '../pictures/2.jpg';
 import logo from '../pictures/Logo.jpg';
+import { Link } from 'react-router-dom';
 
 const Player = ({user}) => (
     <Link to={"/profilepage/" + user.id}> 
-    <div className="player container">
-        <div className="player username">{user.username}</div>
-        <div className="player name">{user.name}</div>
-        <div className="player status">{user.status}</div>
+    <div className={`userlist container ${user.status}`}>
+        <div className="username">{user.username}</div>
+        <div className="name">{user.name}</div>
     </div>
     </Link>
   );
@@ -22,14 +23,7 @@ Player.propTypes = {
 };
 
 const AllUsers = () => {
-    // use react-router-dom's hook to access the history
     const history = useHistory();
-
-    // define a state variable (using the state hook).
-    // if this variable changes, the component will re-render, but the variable will
-    // keep its value throughout render cycles.
-    // a component can have as many state variables as you like.
-    // more information can be found under https://reactjs.org/docs/hooks-state.html
     const [users, setUsers] = useState(null);
     console.log(users)
     const logout = async () => {
@@ -42,34 +36,11 @@ const AllUsers = () => {
         }
     }
 
-
-    // the effect hook can be used to react to change in your component.
-    // in this case, the effect hook is only run once, the first time the component is mounted
-    // this can be achieved by leaving the second argument an empty array.
-    // for more information on the effect hook, please see https://reactjs.org/docs/hooks-effect.html
     useEffect(() => {
-        // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
         async function fetchData() {
             try {
                 const response = await api.get('/users');
-
-                // delays continuous execution of an async operation for 1 second.
-                // This is just a fake async call, so that the spinner can be displayed
-                // feel free to remove it :)
-                await new Promise(resolve => setTimeout(resolve, 1000));
-
-                // Get the returned users and update the state.
                 setUsers(response.data);
-
-                // This is just some data for you to see what is available.
-                // Feel free to remove it.
-                console.log('request to:', response.request.responseURL);
-                console.log('status code:', response.status);
-                console.log('status text:', response.statusText);
-                console.log('requested data:', response.data);
-
-                // See here to get more data.
-                console.log(response);
             } catch (error) {
                 console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
                 console.error("Details:", error);
@@ -79,12 +50,22 @@ const AllUsers = () => {
         fetchData();
     }, []);
 
+    let content = (<div></div>);
 
-
+    if (users) {
+      content = (
+        <div className="game">
+          <ul className="userlist">
+            {users.map(user => (
+              <Player user={user} key={user.id}/>
+            ))}
+          </ul>
+        </div>
+      );
+    }
   
     return (
         <BaseContainer className="landing container">
-
             <div className="landing navbar">
                 <img className="landing img" src={logo} alt="LOL"/>
                 <nav>
@@ -95,7 +76,6 @@ const AllUsers = () => {
                     </ul>
                 </nav>
                 <div className="landing button-container">
-
                     <button
                         className="button"
                         onClick={() => logout()}
@@ -104,6 +84,7 @@ const AllUsers = () => {
                     </button>
                 </div>
             </div>
+            {content}
         </BaseContainer>
     );
 }
