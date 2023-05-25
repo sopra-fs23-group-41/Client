990b7c0c4bbbbaab175031ef70caa6c95dabd1e0
tyagos <126197200+tyagos@users.noreply.github.com>
2023-05-25 15:15:42 +0200

    approuting fixed (maybe)

--- src/components/routing/routeProtectors/LoginGuard.js
+++ src/components/routing/routeProtectors/LoginGuard.js
@@ -1,14 +1,14 @@
-import {Redirect} from "react-router-dom";
+import {Redirect, Route} from "react-router-dom";
 import PropTypes from "prop-types";
 
 export const LoginGuard = props => {
-  if (!localStorage.getItem("token")) {
-    return props.children;
-  }
-  // if user is already logged in, redirects to the main /app
-  return <Redirect to="/registration"/>;
+    if (!localStorage.getItem("token")) {
+        return props.children;
+    }
+    // if user is already logged in, redirects to the main /app
+    return <Route render={() => <Redirect to="/landing"/>}/>;
 };
 
 LoginGuard.propTypes = {
-  children: PropTypes.node
+    children: PropTypes.node
 }
--- src/components/routing/routers/AppRouter.js
+++ src/components/routing/routers/AppRouter.js
@@ -1,6 +1,5 @@
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import {GameGuard} from "components/routing/routeProtectors/GameGuard";
-//import GameRouter from "components/routing/routers/GameRouter";
 import Login from "components/views/Login";
 import Registration from "../../views/Registration";
 import Landing from "../../views/Landing"
@@ -23,7 +22,6 @@ import LobbySingle from "../../views/LobbySingle";
 import GameLoadingBuffer from "../../views/GameLoadingBuffer";
 import MostExpensiveItem from "../../views/MostExpensiveItem";
 import DelayBuffer from "../../views/DelayBuffer";
-
 import {LoginGuard} from "../routeProtectors/LoginGuard";
 import DelayBufferAfterLobby from "../../views/DelayBufferAfterLobby";
 
@@ -37,12 +35,6 @@ const AppRouter = () => {
                         <Login/>
                     </LoginGuard>
                 </Route>
-
-                {/*<Route path="/game">
-                    <GameGuard>
-                        <GameRouter base="/game"/>
-                    </GameGuard>
-                </Route>*/}
 
                 <Route exact path="/profilepage/:id">
                     <GameGuard>
